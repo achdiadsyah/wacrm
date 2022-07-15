25daf6ec3ff3ccb52b6a6d415fad74e6dc819273
Ryan Syah <achdiadsyah@gmail.com>
2022-07-16 05:05:35 +0700

    Import assets and slice template mazer

---
+++ resources/views/blank-content.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.master')
+@push('head-script')
+    
+@endpush
+@section('content')
+
+<div class="page-heading">
+    <h3>{{$title}}</h3>
+</div>
+
+<div class="page-content">
+    <section class="row">
+        {{-- Add Col Here --}}
+    </section>
+</div>
+
+@endsection
+@push('foot-script')
+
+@endpush
